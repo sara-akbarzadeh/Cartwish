354c7922700ca0afffb1dc457602f2175adcf451
Sara <saraakbarzadeh2004@gmail.com>
2024-07-17 19:47:43 +0330

    this is my first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <nav>Navbar</nav>
+      <main>All Routing</main>
+    </div>
+  );
+};
+
+export default App;
