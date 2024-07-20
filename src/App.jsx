0140ce47675deb9552f62d43ed3b33e2b715e9dc
Sara <saraakbarzadeh2004@gmail.com>
2024-07-20 12:58:57 +0330

    product part

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./components/Home/HomePage";
+import "./index.css";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
-      <main>All Routing</main>
+      <main>
+        <HomePage />
+      </main>
     </div>
   );
 };
---
+++ src/components/Home/FeaturedProducts.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./FeaturedProducts.css";
+
+const FeaturedProducts = () => {
+  return (
+    <section className="featured_products">
+      <h2>Featured Products</h2>
+      <div className="align_center featured_products_list">
+        <article className="product_card">Product</article>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedProducts;
