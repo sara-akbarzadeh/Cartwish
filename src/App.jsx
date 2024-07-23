9c4a40c31a55a1dd41bdcf8b7e1ed4ac7e07b82f
Sara <saraakbarzadeh2004@gmail.com>
2024-07-24 01:21:32 +0330

    hook changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import ProductsPage from "./components/Products/ProductsPage";
 import SingleProductPage from "./components/Products/SingleProduct/SingleProductPage";
 import CartPage from "./components/Cart/CartPage";
 import MyOrderPage from "./components/MyOrder/MyOrderPage";
+import LoginPage from "./components/Authentication/LoginPage";
+import SignupPage from "./components/Authentication/SignupPage";
 
 const App = () => {
   return (
@@ -17,7 +19,9 @@ const App = () => {
         {/* <ProductsPage /> */}
         {/* <SingleProductPage /> */}
         {/* <CartPage /> */}
-        <MyOrderPage />
+        {/* <MyOrderPage /> */}
+        {/* <LoginPage /> */}
+        <SignupPage />
       </main>
     </div>
   );
